8ba93b4d3afefba56795e3d6bbb9f9ddd0374668
anonymous-jones <josephjtighecode@gmail.com>
2025-04-02 18:36:13 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# sign up page template
- simple sign up page
+simple sign up page
+make a clone of the repository before editing
+feel free to edit the css, html, ect.
+
+must have node js installed to run the server
+run server by navigating to the root in terminal, and running the command "node server.js"
---
+++ server.js
@@ -0,0 +1,79 @@
+const hbs = require('hbs');
+const express = require('express');
+var bodyParser = require('body-parser')
+const fs = require('fs');
+const path = require('path');
+const app = express();    
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const port = 3000;
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.get('/', urlencodedParser, (req, res) => {
+    res.render('index');
+});
+app.post('/main', urlencodedParser, (req, res) => {
+    fs.readFile("./server/users.json", "utf8", (err, users) => {
+        if (err) {
+          console.log("File read failed:", err);
+          res.redirect('/');
+          return;
+        }
+        users = JSON.parse(users);
+        if (users.users.hasOwnProperty(req.body.user) && users.users[req.body.user] == req.body.pass) {
+            res.render('main', req.body);
+            return;
+        }
+        res.redirect('/signUp');
+    });
+});
+app.post('/save', urlencodedParser, (req, res) => {
+    fs.readFile("./server/users.json", "utf8", (err, saves) => {
+        if (err) {
+            console.log("File read failed:", err);
+            res.redirect('/');
+            return;
+        }
+        saves = JSON.parse(saves);
+        saves.saves[req.body.user] = req.body.save;
+        fs.writeFile("./server/users.json", JSON.stringify(saves), (err) => {
+            if (err) {
+                console.log("File write failed:", err);
+                res.redirect('/');
+                return;
+            }
+            res.redirect('/');
+        });
+    });
+});
+app.post('/signUp', urlencodedParser, (req, res) => {
+    fs.readFile("./server/users.json", "utf8", (err, saves) => {
+        if (err) {
+            console.log("File read failed:", err);
+            res.redirect('/');
+            return;
+        }
+        saves = JSON.parse(saves);
+        saves.users[req.body.user] = req.body.pass;
+        fs.writeFile("./server/users.json", JSON.stringify(saves), (err) => {
+            if (err) {
+                console.log("File write failed:", err);
+                res.redirect('/logIn');
+                return;
+            }
+            res.redirect('/logIn');
+        });
+    });
+});
+app.get('/signUp', (req, res) => {
+    res.render('signUp');
+});
+app.get('/logIn', (req, res) => {
+    res.render('logIn');
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
